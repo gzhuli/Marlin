95bfccb9cf15254c6a37333bc404745c53e44f50
Kyle Hu <kyle.hu.gz@gmail.com>
2020-08-11 16:10:28 +0800

    Merge with 2.0.6

--- Marlin/src/HAL/STM32/timers.cpp
+++ Marlin/src/HAL/STM32/timers.cpp
@@ -76,7 +76,7 @@
   #define MCU_STEP_TIMER  4
   #define MCU_TEMP_TIMER  2
 #elif defined(STM32F401xC) || defined(STM32F401xE)
-  #define MCU_TIMER_RATE (F_CPU / 2)
+  #define MCU_TIMER_RATE (F_CPU)
   #define MCU_STEP_TIMER  9
   #define MCU_TEMP_TIMER 10
 #elif defined(STM32F4xx) || defined(STM32F7xx)
